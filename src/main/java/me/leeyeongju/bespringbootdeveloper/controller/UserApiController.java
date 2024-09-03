90548e1ecd96181ec1063d240d981e7ced834376
vkslvkd092 <vkslvkd092@naver.com>
2024-09-03 15:07:43 +0900

    feat(controller): add user registration endpoint - Implemented `UserApiController` for handling user registration - Added a `POST` endpoint to receive registration requests and save user data - Redirects to the login page upon successful registration
    Closes #25

---
+++ src/main/java/me/leeyeongju/bespringbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,24 @@
+package me.leeyeongju.bespringbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.leeyeongju.bespringbootdeveloper.dto.AddUserRequest;
+import me.leeyeongju.bespringbootdeveloper.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    /*
+    회원 가입과 관련된 요청을 처리하는 컨트롤러
+     */
+    
+    private final UserService userService;
+    
+    // 회원 가입 요청을 받으면 서비스 메서드를 사용해 사용자를 저장한 뒤, 로그인 페이지로 이동하는 메서드
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request); // 회원 가입 메서드 호출
+        return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+}
