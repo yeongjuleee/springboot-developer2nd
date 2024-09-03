e64a6b31ad34a286159aeecc05a926aabdf642fa
vkslvkd092 <vkslvkd092@naver.com>
2024-09-03 15:29:49 +0900

    feat(controller): add view controllers for login and signup
    -Implemented `UserViewController` to handle routes for login and signup pages
    - Added `GET /login` endpoint to return `login.html`
    - Added `GET /signup` endpoint to return `signup.html`
    
    Closes #26

---
+++ src/main/java/me/leeyeongju/bespringbootdeveloper/controller/UserViewController.java
@@ -0,0 +1,21 @@
+package me.leeyeongju.bespringbootdeveloper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+    /*
+    로그인, 회원 가입 경로로 접근하면 뷰 파일을 연결하는 컨트롤러
+     */
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // `/login` 경로로 접근을 하면 `login()` 메서드가 `login.html`로 반환
+    } 
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup"; // `/signup` 경로에 접근하면 `signup()` 메서드는 `signup.html` 으로 반환
+    }
+}
